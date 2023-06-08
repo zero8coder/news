cf47b94ffeae4b2c161b153acc8bf327cf3d935c
zero8coder <s812419396@gmail.com>
2023-06-08 11:31:23 +0800

    update:调整样式

--- resources/views/weibo.blade.php
+++ resources/views/weibo.blade.php
@@ -6,20 +6,41 @@
 </head>
 <style>
     .center {
-        width:400px;
-        margin:0 auto;
+        max-width: 100%;
+        margin: 0 auto;
+        padding: 10px;
+        box-sizing: border-box;
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        text-align: left;
+        padding: 8px;
+        border-bottom: 1px solid #ddd;
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+    a {
+        text-decoration: none;
     }
 </style>
 <body>
 <div class="center">
-<table>
-    <tr><th>排名</th><th>标题</th></tr>
-    @foreach ($weibos as $weibo)
-    <tr>
-        <td>{{$weibo->no}}</td><td><a target="_blank" href="{{$weiboHost}}{{$weibo->url}}">{{$weibo->title}}</a></td>
-    </tr>
-    @endforeach
-</table>
+    <table>
+        <tr>
+            <th>排名</th>
+            <th>标题</th>
+        </tr>
+        @foreach ($weibos as $weibo)
+            <tr>
+                <td>{{$weibo->no}}</td>
+                <td><a target="_blank" href="{{$weiboHost}}{{$weibo->url}}">{{$weibo->title}}</a></td>
+            </tr>
+        @endforeach
+    </table>
 </div>
 </body>
 </html>
